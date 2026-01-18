a6d37d15dad849dccd645a656da94f03599cb622
syarifzidan <syarifzidan29@gmail.com>
2026-01-18 13:47:03 +0700

    added player + circle logic

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
-
+from player import Player
 
 
 def main():
@@ -10,6 +10,9 @@ def main():
     print(f"Screen width: 1280\nScreen height: 720")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while True:
         log_state()
         for event in pygame.event.get():
@@ -18,7 +21,12 @@ def main():
             pass
         screen.fill("black")
         
+        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        pygame.time.Clock()
+        dt = clock.tick(60)/1000
     
 
 
