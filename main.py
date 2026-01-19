72ce53d902cd6d686961dc10542e9fe28bbfbeac
syarifzidan <syarifzidan29@gmail.com>
2026-01-19 10:02:49 +0700

    added player movement + moving asteroid sprites

--- main.py
+++ main.py
@@ -2,17 +2,26 @@ import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 from player import Player
-
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: 1280\nScreen height: 720")
+    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    
+    asteroid_field = AsteroidField()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
@@ -21,12 +30,14 @@ def main():
             pass
         screen.fill("black")
         
-        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-        player.update(dt)
-        player.draw(screen)
+        
+        updatable.update(dt)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
         pygame.time.Clock()
         dt = clock.tick(60)/1000
+        
     
 
 
